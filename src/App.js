7412b92030f81e8b7e484f5c495d91f24346e0fb
Adrian Gajda <adrian.gajda@hotmail.com>
2021-05-08 16:03:37 +0100

    setup income inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import Header from "./components/Header";
+import ItemList from "./components/ItemList";
+import { useState, useEffect } from "react";
+
+const values = "Monthly Wages";
 
 function App() {
+  const [incomeValue, setIncomeValue] = useState(0);
+  const [incomes, setIncomes] = useState(0);
+  const [test, setTest] = useState(0);
+  let result = 0;
+
+  const [inputIncomes, setInputIncomes] = useState([0, 0]);
+
+  const onInputChange = (id, value) => {
+    value === "" ? (inputIncomes[id - 1] = 0) : (inputIncomes[id - 1] = value);
+
+    console.log("initial array" + inputIncomes[0] + inputIncomes[1]);
+
+    console.log(id);
+    console.log(value);
+    console.log(inputIncomes[0]);
+    console.log(inputIncomes[1]);
+
+    setTest(parseInt(inputIncomes[0]) + parseInt(inputIncomes[1]));
+  };
+
   return (
     <div className="content-wrapper h-screen w-screen">
+      <h1 className="px-12 py-4">Budget Tracker App</h1>
       <div className="container mx-auto py-10 px-8 border">
-        <Header />
-        <div className="app-body">
-          <h2 className="font-bold">Body</h2>
+        <div>
+          <div className="main-header flex justify-center border-b">
+            <button>Click</button>
+            <button>Click</button>
+            <button>Click</button>
+            <button>Click</button>
+          </div>
         </div>
+        <div className="app-body">
+          <h2>Income {test}</h2>
+          <ItemList onInputChange={onInputChange} id={1} />
+
+          <ItemList onInputChange={onInputChange} id={2} />
+        </div>
+        <p>
+          ID 2 value {inputIncomes[0]}// {result}
+        </p>
       </div>
     </div>
   );
---
+++ src/components/ItemList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const ItemList = ({ id, onInputChange }) => {
+  const [inputValue, setInputValue] = useState(0);
+
+  useEffect(() => {
+    onInputChange(id, inputValue);
+  }, [inputValue]);
+
+  return (
+    <div>
+      <h3>Title</h3>
+      <div class="flex">
+        <div class="text-sm border border-2 rounded-l px-10 py-2 bg-gray-300 whitespace-nowrap">
+          <div></div>
+        </div>
+        <input
+          name="field_name"
+          class="border border-2 rounded-r px-4 py-2 w-full"
+          type="number"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
